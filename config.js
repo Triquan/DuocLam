7d4220bd2b1d4d8afed57381ea3fc5f6df659874
Triquan <117756186+Triquan@users.noreply.github.com>
2022-11-08 23:21:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 9h, nhắn tin chơi nha',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/messages/t/100013224910533' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100062867741874' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
